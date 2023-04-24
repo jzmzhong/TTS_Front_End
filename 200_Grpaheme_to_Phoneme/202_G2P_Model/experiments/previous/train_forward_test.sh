a084e6261790dc2ee505b9f1c27c796122a44931
jzmzhong <jinzuomuzhong@gmail.com>
2023-04-24 21:10:34 +0800

    [202_G2P_Model] Previous files reorganized.

--- 200_Grpaheme_to_Phoneme/202_G2P_Model/experiments/previous/train_forward_test.sh
+++
@@ -1,7 +0,0 @@
-PORJECT_DIR=`pwd`/..
-
-TRAIN_PATH=$PORJECT_DIR/datasets/3_train_and_eval_data/EnUs/EnUs_dict_exclude_polyphone_train.txt
-VALID_PATH=$PORJECT_DIR/datasets/3_train_and_eval_data/EnUs/EnUs_dict_exclude_polyphone_valid.txt
-CONFIG_PATH=$PORJECT_DIR/dp/configs/forward_config_V1.0.yaml
-
-CUDA_VISIBLE_DEVICES=0 python3 $PORJECT_DIR/scripts/run_training.py --train-path $TRAIN_PATH --valid-path $VALID_PATH --config-path $CONFIG_PATH
